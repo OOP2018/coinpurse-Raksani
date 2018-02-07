f7c6bbc9e389e6e68175f387a60b8082b632fcf1
Raksani Khunamas <raksani.k@ku.th>
2018-02-07 21:19:30 +0700

    issue fixed

--- src/coinpurse/MoneyUtill.java
+++ src/coinpurse/MoneyUtill.java
@@ -12,63 +12,66 @@ public class MoneyUtill {
 	 * 
 	 */
 	/*
-	 * To print the whole list of coins.
-	 * @param coins is the list of the coin related with Coin class.
+	 * To print the whole list of valuables.
+	 * @param valuables is the list of the coin related with Coin class.
 	 */
-	public static List<Coin> coins = new ArrayList<Coin>();
+	public static List<Valuable> valuables = new ArrayList<Valuable>();
 
-	public static void printCoins(List<Coin> coin) {
-		for (Coin coins : coin) {
-			System.out.printf("\t %s \n", coins.toString());
+	public static void printCoins(List<Valuable> valuables) {
+		for (Valuable valuable : valuables) {
+			System.out.printf("\t %s \n", valuable.toString());
 		}
 
 	}
 
 	/*
-	 * To sort the coins. Order by min to max value.
+	 * To sort the valuables. Order by min to max value.
 	 * 
-	 * @param coins is the list of the coin related with Coin class.
+	 * @param valuables is the list of the coin related with Coin class.
 	 */
 	public static void sortCoins() {
+		ValueComparator comp = new ValueComparator();
 		// This static method sorts any list of Comparable objects
-		java.util.Collections.sort(coins);
+		java.util.Collections.sort(valuables,comp);
 	}
 
 	/**
 	 * Filter the coin by use their currency.
 	 * 
-	 * @param coins
+	 * @param valuables
 	 *            is the list of the coin related with Coin class.
 	 * @param currency
 	 *            is the currency of each value, can be various.
 	 * @return the list after sort and filter by currency.
 	 */
-
-	public static List<Coin> filterByCurrency(List<Coin> coins, String currency) {
-		List<Coin> filteredByCurrencyCoins = new ArrayList<Coin>();
-		for (Coin coin : coins) {
-			if (coin.getCurrency().equals(currency)) {
-				filteredByCurrencyCoins.add(coin);
+	public static List<Valuable> filterByCurrency(List<Valuable> valuables, String currency) {
+		List<Valuable> filteredByCurrencyCoins = new ArrayList<Valuable>();
+		for (Valuable valuable : valuables) {
+			if (valuable.getCurrency().equals(currency)) {
+				filteredByCurrencyCoins.add(valuable);
 			}
 		}
 		return filteredByCurrencyCoins;
 	}
 
 	public static void main(String[] args) {
-		coins.add(new Coin(0.25, "Baht"));
-		coins.add(new Coin(2.0, "Baht"));
-		coins.add(new Coin(1.0, "Baht"));
-		coins.add(new Coin(10.0, "Baht"));
-		coins.add(new Coin(0.50, "Won"));
-		coins.add(new Coin(5.0, "Baht"));
-		coins.add(new Coin(10.0, "Won"));
+		valuables.add(new BankNote(20,"Bath"));
+		valuables.add(new BankNote(100,"Bath"));
+		valuables.add(new BankNote(40,"Bath"));
+		valuables.add(new Coin(0.25, "Baht"));
+		valuables.add(new Coin(2.0, "Baht"));
+		valuables.add(new Coin(1.0, "Baht"));
+		valuables.add(new Coin(10.0, "Baht"));
+		valuables.add(new Coin(0.50, "Won"));
+		valuables.add(new Coin(5.0, "Baht"));
+		valuables.add(new Coin(10.0, "Won"));
 		//print the original coin's list.
-		printCoins(coins);
-		//sort the coins.
+		printCoins(valuables);
+		//sort the valuables.
 		sortCoins();
 		System.out.println("After Sort");
-		printCoins(coins); // the coins should be sorted by value now.
-		filterByCurrency(coins, "Baht");
-		printCoins(coins); // the coins should be filtered by currency.
+		printCoins(valuables); // the valuables should be sorted by value now.
+		filterByCurrency(valuables, "Baht");
+		printCoins(valuables); // the valuables should be filtered by currency.
 	}
 }
