6c9bf1001a19ecfa488c59fb4056ca0dc53f39a5
Raksani <raksani.k@ku.th>
2018-02-17 14:29:07 +0700

    finished demo

--- src/coinpurse/MoneyFactoryDemo.java
+++ src/coinpurse/MoneyFactoryDemo.java
@@ -1,42 +1,30 @@
 package coinpurse;
 
 public class MoneyFactoryDemo {
-    private static void setFactory(String factoryClass){
-        MoneyFactory factory = null;
-        try{
-            factory = (MoneyFactory) Class.forName(factoryClass).newInstance();
-        } catch (IllegalArgumentException | InstantiationException | ClassNotFoundException | IllegalAccessException e) {
-            throw new IllegalArgumentException("Factory name is invalid");
-        }
-        MoneyFactory.setFactory(factory);
-    }
+    public static void main(String[]args) {
+        /**
+         * Thai factory.
+         */
+        MoneyFactory.setFactory(new ThaiMoneyFactory());
+        MoneyFactory thaiFactory = MoneyFactory.getInstance();
+        Valuable thaiM = thaiFactory.createMoney(0.50);
+        System.out.println(thaiM.toString());
+        Valuable thaiM2 = thaiFactory.createMoney("100.0");
+        System.out.println(thaiM2.toString());
+        Valuable thaiM3 = thaiFactory.createMoney("500.0");
+        System.out.println(thaiM3.toString());
 
-    private static Money testCreateThaiCurrency(double value){
-        if(value == 0.25 || value == 0.50 || value == 1 || value ==2 || value ==5 || value == 10 ) return new Coin(value,"Baht");
-        else if (value == 20 || value == 50 || value == 100 || value == 500 || value == 1000) return new BankNote(value,"Baht");
-        else throw new IllegalArgumentException("Something wrong with creating " + value + "Baht");
-    }
+        /**
+         * Malay factory.
+         */
+        MoneyFactory.setFactory(new MalayMoneyFactory());
+        MoneyFactory malayFactory = MoneyFactory.getInstance();
+        Valuable malayM = malayFactory.createMoney(0.10);
+        System.out.println(malayM.toString());
+        Valuable malayM2 = malayFactory.createMoney("1.0");
+        System.out.println(malayM2.toString());
+        Valuable malayM3 = malayFactory.createMoney("5.0");
+        System.out.println(malayM3.toString());
 
-    private static Money testCreateMalayCurrency(double value){
-        if(value == 0.05 || value == 0.10 || value == 0.20 || value == 0.50) return new Coin(value, "Sen");
-        else if (value == 1 || value ==2 || value ==5 || value == 10 || value == 20 || value == 50 || value == 100) return new BankNote(value,"Ringgit");
-        else throw new IllegalArgumentException("Something wrong with creating " + value + "Ringgit");
-    }
-
-    private static void testCase(String testPrompt, Object expected, Object actual){
-        System.out.println(testPrompt + "test");
-        System.out.printf("Expected: %s, Actual: %s, Result: %s%n",expected,actual,expected.equals(actual));
-    }
-
-    private static void testThaiMoney(double[] amounts){
-        //setFactory("coinpurse,ThaiMoneyFactory");
-        MoneyFactory moneyFactory = MoneyFactory.getInstance();
-        System.out.println("Test creating money.");
-        for (double a : amounts ){
-        }
-    }
-
-    public static void main(String[] args){
-        double[] amounts = {1,2};
     }
 }
