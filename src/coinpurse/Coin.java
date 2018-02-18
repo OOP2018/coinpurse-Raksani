a60532a0cc2b1e47e7d1c759027ace19cdd79a35
Raksani <raksani.k@ku.th>
2018-02-18 18:21:35 +0700

    add javadoc in Coin

--- src/coinpurse/Coin.java
+++ src/coinpurse/Coin.java
@@ -2,18 +2,21 @@ package coinpurse;
 
 /**
  * Coin represents coinage (money) with a fixed value and currency.
- * 
+ *
  * @author Raksani Khunamas.
  */
 public class Coin extends Money {
-	
+    /**
+     * @param value    of entered money.
+     * @param currency entered money.
+     */
 
-	public Coin(double value, String currency) {
-		super(value, currency);
-	}
+    public Coin(double value, String currency) {
+        super(value, currency);
+    }
 
-	@Override
-	public String toString() {
-		return this.getValue() + "-" + this.getCurrency();
-	}
+    @Override
+    public String toString() {
+        return this.getValue() + "-" + this.getCurrency();
+    }
 }
